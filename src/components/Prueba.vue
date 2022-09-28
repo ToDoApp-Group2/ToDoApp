1d3e6994ea597564e0a119d9df874c477b7fe23a
JReuman1 <99495156+JReuman1@users.noreply.github.com>
2022-09-28 10:02:38 -0400

    Added call data function

--- src/components/Prueba.vue
+++ src/components/Prueba.vue
@@ -1,6 +1,6 @@
 <template>
-  <h1>
-  {{this.tasks.data}}</h1>
+  <h1>{{this.statuses.data}}
+    {{this.tasks.data}}</h1>
 </template>
 
 <script>
@@ -14,20 +14,34 @@ export default {
             user : supabase.auth.user(),
             tasks: [],
             //userid : user.id,
-
+            //task : "",
+            statuses : [],
         }
 
     },
     methods: {
         async callTasks() {
            this.tasks = await supabase.from('tasks').select('title', 'is_complete').eq('user_id', this.user.id)
-        }
+        },
+
+        async callStatus() {
+            this.statuses = await supabase.from('tasks').select('is_complete').eq('user_id', this.user.id)
+        },
+
+        async CallData() {
+            this.tasks = await supabase.from('tasks').select('title', 'is_complete').eq('user_id', this.user.id);
+            this.statuses = await supabase.from('tasks').select('is_complete').eq('user_id', this.user.id)
+        },
+
+        /*deleteTask(index){
+            this.tasks.splice(index, 1);*/
     },
     mounted(){
-        this.callTasks()
+        this.CallData()
     }
 
 }
+
 </script>
 
 <style>
